e889c8ebdd540d10ce500370c26ad77fc0c56fd9
Maximiliano Usich <maximilianou@gmail.com>
2021-06-25 17:41:39 -0300

    feat(eth): ethereum local env, react connect metamask

--- README.md
+++ README.md
@@ -4,12 +4,65 @@
 
 ---
 ---
-[wip]
+[WIP]
 
 
 ---
 ---
 #### Smart Contract, DApp Decentrilized web, web3 
+
+```js
+import { useState } from 'react';
+import { ethers } from 'ethers'
+import './App.css';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+const greeterAddress = '0x5....................................';
+function App() {
+  const [greeting, setGreetingValue] = useState('');
+  const requestAccount = async () => {
+    await window.ethereum.request({ method: 'eth_requestAccounts'});
+  }
+  const fetchGreeting = async () => {
+    if( typeof window.ethereum !== 'undefined'){
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
+      try{
+        const data = await contract.greet();
+        console.log('data: ', data);
+      }catch(err){
+        console.log('Error: ', err);
+      }
+    }
+  }
+  const setGreeting = async () => {
+    if(!greeting) return;
+    if(typeof window.ethereum !== 'undefined'){
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      setGreetingValue('');
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
+  return (
+    <div className="App">
+      <section className="App-header">
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input onChange={e => setGreetingValue(e.target.value)} 
+               placeholder='Set greeting' 
+               value={greeting}
+        />
+      </section>
+    </div>
+  );
+}
+export default App;
+```
+
 - Makefile
 ```yaml
 ## dapp
--- dapp/ui/src/App.js
+++ dapp/ui/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { ethers } from 'ethers'
 import './App.css';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 
+const greeterAddress = '0x5';
 function App() {
+  const [greeting, setGreetingValue] = useState('');
+  const requestAccount = async () => {
+    await window.ethereum.request({ method: 'eth_requestAccounts'});
+  }
+  const fetchGreeting = async () => {
+    if( typeof window.ethereum !== 'undefined'){
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
+      try{
+        const data = await contract.greet();
+        console.log('data: ', data);
+      }catch(err){
+        console.log('Error: ', err);
+      }
+
+    }
+  }
+  const setGreeting = async () => {
+    if(!greeting) return;
+    if(typeof window.ethereum !== 'undefined'){
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      setGreetingValue('');
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-header">
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input onChange={e => setGreetingValue(e.target.value)} 
+               placeholder='Set greeting' 
+               value={greeting}
+        />
+      </section>
     </div>
   );
 }
